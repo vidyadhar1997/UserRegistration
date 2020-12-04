cc18f7420d9501e7846190b39e19dfae4a6d6b14
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-04 16:49:18 +0530

    [vidyadhar hudge]Add:Validating first name

--- UserRegistrationUsingRegex/Program.cs
+++ UserRegistrationUsingRegex/Program.cs
@@ -7,6 +7,10 @@ namespace UserRegistrationUsingRegex
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to the user registration problem");
+            Console.WriteLine("Enter the first name = ");
+            string firstName = Console.ReadLine();
+            UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+            Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
         }
     }
 }
---
+++ UserRegistrationUsingRegex/UserRegistrationRegex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingRegex
+{
+    class UserRegistrationRegex
+    {
+        public static string VALIDNAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validateFirstName(String firstName)
+        {
+            return Regex.IsMatch(firstName, VALIDNAMEPATTERN);
+        }
+    }
+}
