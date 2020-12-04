d6780f71f6ec00ecff42211c56fa2e1e3ab55efd
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-04 18:31:29 +0530

    [vidyadhar hudge]Add:At least one special character

--- UserRegistrationUsingRegex/UserRegistrationRegex.cs
+++ UserRegistrationUsingRegex/UserRegistrationRegex.cs
@@ -10,7 +10,7 @@ namespace UserRegistrationUsingRegex
         public static string VALIDNAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
         public static string EMAILPATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
         public static string MOBILEPATTERN = "^[0-9]{2}[ ][0-9]{10}$";
-        public static string PASSWORDRULE = "^[A-Z]{1}[0-9]{1}[a-z]{6,}";
+        public static string PASSWORDRULE = "^(?=.*[0 - 9])(?=.*[a - z])(?=.*[A - Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
         public bool validateFirstName(String firstName)
         {
             return Regex.IsMatch(firstName, VALIDNAMEPATTERN);
